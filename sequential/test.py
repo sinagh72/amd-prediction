3a1bee002d9bd6e3246e6b697120530f80ef5b17
sinagh72 <sina.gholami72@gmail.com>
2023-01-25 16:35:29 -0500

    prepare results

--- sequential/test.py
+++ sequential/test.py
@@ -7,8 +7,9 @@ import matplotlib.pyplot as plt
 from data_prepration import testing_data, testing_data2, testaugmentation
 import keras as k
 from ModelTraining import create_model
+import shutil
 
-BASE_DIR = 'adam_fl_pre_remainder (default)/'
+BASE_DIR = 'paper_percentage/p-0.30000000000000004/'
 DATA_DIR = '../data/'
 f = 0  # type of optimizer: 0-adam 1-RMSprop
 padding = "pre"
@@ -72,83 +73,89 @@ for m in mon:
     best_NN = NN[ind]
     #
     best_fold = np.argmax(ROC_AUC[ind * len(FOLDS):ind * len(FOLDS) + len(FOLDS)])
-    slen = SLEN[ind * len(FOLDS):ind * len(FOLDS) + len(FOLDS)][best_fold]
-    #
-    best_combo = [best_NN, FOLDS[best_fold], slen]
-    print('[NN, fold, slen]: ', best_combo)
 
-    # test on test dataset
-    patients_vec_test, patients_label_test, Seq_len_test = testing_data(test, strm, slen)
-    # slen = 57
-    # slen = max(Seq_len_test)
-    # print('max #visit: ', slen)
-    # new_patients_vec, new_patients_label = testaugmentation(patients_vec_test, patients_label_test, 0)
+    for fold in range(len(FOLDS)):
+        slen = SLEN[ind * len(FOLDS):ind * len(FOLDS) + len(FOLDS)][fold]
+        #
+        best_combo = [best_NN, FOLDS[fold], slen]
+        print('[NN, fold, slen]: ', best_combo)
 
-    X_test = pad_sequences(patients_vec_test, slen, padding=padding, truncating=padding, value=0, dtype='float32')
-    Y_test = pad_sequences(patients_label_test, slen, padding=padding, truncating=padding, value=2.)
-    # X_test = np.asarray(patients_vec_test)
-    # Y_test = np.asarray(patients_label_test)
+        # test on test dataset
+        patients_vec_test, patients_label_test, Seq_len_test = testing_data(test, strm, slen)
+        # slen = 57
+        # slen = max(Seq_len_test)
+        # print('max #visit: ', slen)
+        # new_patients_vec, new_patients_label = testaugmentation(patients_vec_test, patients_label_test, 0)
 
-    Y_categorical_test = k.utils.np_utils.to_categorical(Y_test, 3)
-    Y_categorical_test = Y_categorical_test.reshape(Y_test.shape[0], Y_test.shape[1], 3)
+        X_test = pad_sequences(patients_vec_test, slen, padding=padding, truncating=padding, value=0, dtype='float32')
+        Y_test = pad_sequences(patients_label_test, slen, padding=padding, truncating=padding, value=2.)
+        # X_test = np.asarray(patients_vec_test)
+        # Y_test = np.asarray(patients_label_test)
 
-    y_test = Y_categorical_test
+        Y_categorical_test = k.utils.np_utils.to_categorical(Y_test, 3)
+        Y_categorical_test = Y_categorical_test.reshape(Y_test.shape[0], Y_test.shape[1], 3)
 
-    # bestmodel = model
-    latest_file = BASE_DIR + 'models/OCT_model_with_weights_' + str(m) + '_' + str(best_combo[0]) + '_' + str(
-        best_combo[1]) + '.h5'
-    print(latest_file)
-    num_features = X_test.shape[2]
+        y_test = Y_categorical_test
 
-    bestmodel = create_model(slen, num_features, best_combo[0])
-    bestmodel.load_weights(latest_file)
+        # bestmodel = model
+        model_name = 'models/OCT_model_with_weights_' + str(m) + '_' + str(best_combo[0]) + '_' +\
+                     str(best_combo[1]) + '.h5'
+        latest_file = BASE_DIR + model_name
+        print(latest_file)
+        shutil.copy(latest_file, f"../all models/{str(m)}/{model_name}")
+        num_features = X_test.shape[2]
 
-    batch_size = slen
-    preds = bestmodel.predict(X_test, batch_size=batch_size)
-    # convert to the (Batch*Visit, one_hot)
-    y_pred = preds.reshape(X_test.shape[0] * slen, 3)
-    y_true = y_test.reshape(X_test.shape[0] * slen, 3)
+        bestmodel = create_model(slen, num_features, best_combo[0])
+        bestmodel.load_weights(latest_file)
 
-    save_pd = pd.DataFrame(y_pred).reset_index(drop=True)
-    save_pd.rename(columns={'0': '0_pred', '1': '1_pred', '2': '2_pred'}, inplace=True)
-    save_pd = pd.concat([save_pd, pd.DataFrame(y_true).reset_index(drop=True)], axis=1)
-    save_pd.rename(columns={'0': '0_true', '1': '1_true', '2': '2_true'}, inplace=True)
+        batch_size = slen
+        preds = bestmodel.predict(X_test, batch_size=batch_size)
+        # convert to the (Batch*Visit, one_hot)
+        y_pred = preds.reshape(X_test.shape[0] * slen, 3)
+        y_true = y_test.reshape(X_test.shape[0] * slen, 3)
 
-    filepath = 'scores.xlsx'
-    if not os.path.exists(filepath):
-        with pd.ExcelWriter(filepath, engine='openpyxl', mode='w') as writer:
-            save_pd.to_excel(writer, sheet_name="month " + str(m) + " outcome", index=False)
+        # save the scores in the Excel file
+        # save_pd = pd.DataFrame(y_pred).reset_index(drop=True)
+        # save_pd.rename(columns={'0': '0_pred', '1': '1_pred', '2': '2_pred'}, inplace=True)
+        # save_pd = pd.concat([save_pd, pd.DataFrame(y_true).reset_index(drop=True)], axis=1)
+        # save_pd.rename(columns={'0': '0_true', '1': '1_true', '2': '2_true'}, inplace=True)
+        #
+        # filepath = 'scores.xlsx'
+        # if not os.path.exists(filepath):
+        #     with pd.ExcelWriter(filepath, engine='openpyxl', mode='w') as writer:
+        #         save_pd.to_excel(writer, sheet_name="month " + str(m) + " outcome", index=False)
+        #
+        # else:
+        #     with pd.ExcelWriter(filepath, engine='openpyxl', mode='a') as writer:
+        #         save_pd.to_excel(writer, sheet_name="month " + str(m) + " outcome", index=False)
+        # y_true_df.to_excel(filepath, sheet_name="month " + str(m) + " outcome", startcol=4, index=False)
 
-    else:
-        with pd.ExcelWriter(filepath, engine='openpyxl', mode='a') as writer:
-            save_pd.to_excel(writer, sheet_name="month " + str(m) + " outcome", index=False)
-    # y_true_df.to_excel(filepath, sheet_name="month " + str(m) + " outcome", startcol=4, index=False)
+        # select those that index 2 is 0 (in one-hot encoding the y is 0) and
+        y_true_categorical = y_true[y_true[:, 2] == 0, 1]
+        y_pred_score = y_pred[y_true[:, 2] == 0, 1]
 
-    # select those that index 2 is 0 (in one-hot encoding the y is 0) and
-    y_true_categorical = y_true[y_true[:, 2] == 0, 1]
-    y_pred_score = y_pred[y_true[:, 2] == 0, 1]
+        ## ROC with out padding
+        fpr, tpr, thresholds = roc_curve(y_true_categorical, y_pred_score, pos_label=1)
+        roc_auc = auc(fpr, tpr)
 
-    ## ROC with out padding
-    fpr, tpr, thresholds = roc_curve(y_true_categorical, y_pred_score, pos_label=1)
-    roc_auc = auc(fpr, tpr)
+        lr_precision, lr_recall, _ = precision_recall_curve(y_true_categorical, y_pred_score)
+        lr_auc = auc(lr_recall, lr_precision)
 
-    lr_precision, lr_recall, _ = precision_recall_curve(y_true_categorical, y_pred_score)
-    lr_auc = auc(lr_recall, lr_precision)
+        print('AUC and lr_AUC: ', roc_auc, lr_auc)
 
-    print('AUC and lr_AUC: ', roc_auc, lr_auc)
+        ## Plot teh ROC curve
 
-    ## Plot teh ROC curve
+        plt.figure()
+        lw = 2
+        plt.plot(fpr, tpr, color='darkorange',
+                 lw=lw, label='ROC curve for ' + str(m) + 'months (area = %0.4f)' % roc_auc)
 
-    plt.figure()
-    lw = 2
-    plt.plot(fpr, tpr, color='darkorange',
-             lw=lw, label='ROC curve for ' + str(m) + 'months (area = %0.4f)' % roc_auc)
-
-    plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
-    plt.xlim([-0.01, 1.0])
-    plt.ylim([0.0, 1.05])
-    plt.xlabel('False Positive Rate')
-    plt.ylabel('True Positive Rate')
-    plt.title('ROC curves for short and long term prediction')
-    plt.legend(loc="lower right")
-    plt.show()
+        plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
+        plt.xlim([-0.01, 1.0])
+        plt.ylim([0.0, 1.05])
+        plt.xlabel('False Positive Rate')
+        plt.ylabel('True Positive Rate')
+        plt.title('ROC curves for short and long term prediction')
+        plt.legend(loc="lower right")
+        # plt.show()
+        plt.savefig(f"../all models/{str(m)}/results/month_{str(m)}_fold_{str(best_combo[1])}.png")
